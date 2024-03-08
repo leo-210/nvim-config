4cdf42eaf357b512da7a7ffc669c3afe00302918
Léo-21 <leo.kosman@proton.me>
2024-03-08 14:54:58 +0100

    edit some keybindings

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -1,3 +1,7 @@
 require("nvim-tree").setup()
+local api = require("nvim-tree.api")
 
-vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<cr>")
+vim.keymap.set("n", "<leader>ft", function ()
+    api.tree.open({ find_file = true })
+end)
+vim.keymap.set("n", "<leader>fc", api.tree.close_in_this_tab)
